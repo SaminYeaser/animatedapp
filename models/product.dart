7c13af5a24e5f5e5006f0e159f7e0a1ad50fee4f
SaminYeaser <saminyeaser1@gmail.com>
2021-08-25 17:14:48 +0600

    Initial commit

---
+++ models/product.dart
@@ -0,0 +1,13 @@
+class Product{
+  final String? title, image;
+
+  Product({this.title, this.image});
+
+  List<Product> demo_product = [
+    Product(title: 'Cabage',image: 'assets/images/img_1.png'),
+    Product(title: 'Bracoli',image: 'assets/images/img_2.png'),
+    Product(title: 'Carrot',image: 'assets/images/img_3.png'),
+    Product(title: 'Pakcoy',image: 'assets/images/img_3.png'),
+    Product(title: 'Cucumber',image: 'assets/images/img_1.png'),
+  ];
+}
