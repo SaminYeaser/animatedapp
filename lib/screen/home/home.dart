d422d5fab7d3b74e16c22c563409525c4ef5ad9c
SaminYeaser <saminyeaser1@gmail.com>
2021-09-06 22:15:13 +0600

    some work done

--- lib/screen/home/home.dart
+++ lib/screen/home/home.dart
@@ -8,85 +8,91 @@ import 'package:animatedapp/models/product.dart';
 class HomeScreen extends StatelessWidget {
   final controller = HomeController();
 
-  void _onVerticalGesture(DragUpdateDetails details){
-    if(details.primaryDelta! < -0.7){
+  void _onVerticalGesture(DragUpdateDetails details) {
+    if (details.primaryDelta! < -0.7) {
       controller.changeHomeState(HomeState.cart);
-    }else if(details.primaryDelta! > 12){
+    } else if (details.primaryDelta! > 12) {
       controller.changeHomeState(HomeState.normal);
     }
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        bottom: false,
+          bottom: false,
           child: Container(
-        color: Color(0xFFEAEAEA),
-        child: AnimatedBuilder(
-          animation: controller,
-          builder: (context, _) {
-            return LayoutBuilder(
-              builder: (context, BoxConstraints constraints) {
-                return Stack(
-                  children: [
-                    Positioned(
-                      top: 0,
-                      right: 0,
-                      left: 0,
-                      height: headerHeight,
-                      child: HomeHeader(),
-                    ),
-                    Positioned(
-                      top: headerHeight,
-                      left: 0,
-                      right: 0,
-                      height: constraints.maxHeight - headerHeight - cartBarHeight,
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.only(
-                                bottomLeft: Radius.circular(defaultPadding * 1.5),
-                                bottomRight: Radius.circular(defaultPadding * 1.5))),
-                        child:
-                        GridView.builder(
-                          itemCount: demo_product.length,
-                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 2,
-                              childAspectRatio: 0.75,
-                              mainAxisSpacing: defaultPadding,
-                              crossAxisSpacing: defaultPadding),
-                          itemBuilder: (context, index) {
-                            return
-                              ProductCard(
-                              product: demo_product[index],
-                              press: () {},
-                            );
-                          },
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 0,
-                      left: 0,
-                      right: 0,
-                      height: cartBarHeight,
-                      child: GestureDetector(
-                        onVerticalDragUpdate: _onVerticalGesture,
-                        child: Container(
-                          color: Colors.red,
-                        ),
-                      ),
-                    )
-                  ],
-                );
-              }
-            );
-          }
-        ),
-      )),
+              color: Color(0xFFEAEAEA),
+              child: AnimatedBuilder(
+                  animation: controller,
+                  builder: (context, _) {
+                    return LayoutBuilder(
+                        builder: (context, BoxConstraints constraints) {
+                      return Stack(
+                        children: [
+                          AnimatedPositioned(
+                            duration: panelTransition,
+                            top: controller.homeState == HomeState.normal
+                                ? headerHeight
+                                : constraints.maxHeight - cartBarHeight * 2 - headerHeight,
+                            left: 0,
+                            right: 0,
+                            height: constraints.maxHeight -
+                                headerHeight -
+                                cartBarHeight,
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: defaultPadding),
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.only(
+                                      bottomLeft:
+                                          Radius.circular(defaultPadding * 1.5),
+                                      bottomRight: Radius.circular(
+                                          defaultPadding * 1.5))),
+                              child: GridView.builder(
+                                itemCount: demo_product.length,
+                                gridDelegate:
+                                    SliverGridDelegateWithFixedCrossAxisCount(
+                                        crossAxisCount: 2,
+                                        childAspectRatio: 0.75,
+                                        mainAxisSpacing: defaultPadding,
+                                        crossAxisSpacing: defaultPadding),
+                                itemBuilder: (context, index) {
+                                  return ProductCard(
+                                    product: demo_product[index],
+                                    press: () {},
+                                  );
+                                },
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            bottom: 0,
+                            left: 0,
+                            right: 0,
+                            height: cartBarHeight,
+                            child: GestureDetector(
+                              onVerticalDragUpdate: _onVerticalGesture,
+                              child: Container(
+                                color: Colors.red,
+                              ),
+                            ),
+                          ),
+                          AnimatedPositioned(
+                            duration: panelTransition,
+                            top: controller.homeState == HomeState.normal
+                                ? 0
+                                : -headerHeight,
+                            right: 0,
+                            left: 0,
+                            height: headerHeight,
+                            child: HomeHeader(),
+                          ),
+                        ],
+                      );
+                    });
+                  }))),
     );
   }
 }
